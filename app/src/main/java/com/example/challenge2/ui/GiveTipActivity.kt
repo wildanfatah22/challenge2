2d64c106255419322e595d80915531aa14ec8e17
wildanfatah22 <awildanfatahillah@gmail.com>
2023-09-30 23:41:08 +0700

    yeay finish

--- app/src/main/java/com/example/challenge2/ui/GiveTipActivity.kt
+++ app/src/main/java/com/example/challenge2/ui/GiveTipActivity.kt
@@ -2,6 +2,8 @@ package com.example.challenge2.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.MenuItem
 import com.example.challenge2.R
 import com.example.challenge2.databinding.ActivityGiveTipBinding
@@ -16,6 +18,43 @@ class GiveTipActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.edtValueInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                calculateTip(s.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+        binding.rgOption.setOnCheckedChangeListener { _, _ ->
+            binding.edtValueInput.text?.let { text ->
+                calculateTip(text.toString())
+            }
+        }
+
+    }
+
+    private fun calculateTip(billAmountText: String) {
+        if (billAmountText.isNotEmpty()) {
+            val billAmount = billAmountText.toDouble()
+            val tipPercentage = when (binding.rgOption.checkedRadioButtonId) {
+                binding.radio20.id -> 0.20
+                binding.radio15.id -> 0.15
+                binding.radio10.id -> 0.10
+                else -> 0.0
+            }
+            val tipAmount = billAmount * tipPercentage
+            val totalAmount = billAmount + tipAmount
+
+            binding.result.text = getString(R.string.tip_result, tipAmount.toString(), totalAmount.toString())
+        } else {
+            binding.result.text = ""
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
