4908873ed4ed94b12b837a86c33fd3dfc34e0776
wildanfatah22 <awildanfatahillah@gmail.com>
2023-09-30 18:28:03 +0700

    layouting, blm logic

--- app/src/main/java/com/example/challenge2/MainActivity.kt
+++ app/src/main/java/com/example/challenge2/MainActivity.kt
@@ -1,11 +1,103 @@
 package com.example.challenge2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.challenge2.adapter.CategoryAdapter
+import com.example.challenge2.adapter.TransactionAdapter
+import com.example.challenge2.data.Category
+import com.example.challenge2.data.Transaction
+import com.example.challenge2.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CategoryAdapter.CategoryItemClickListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        bindTransaction()
+        bindCategory()
+
     }
+
+    private fun bindTransaction(){
+        val rvTransaction = binding.rvTransaction
+        val transaction: List<Transaction> = listOf(
+            Transaction(
+                id = 1,
+                transactionName = "Tip",
+                userName = "John Doe",
+                date = "2023-09-18",
+                message = "Thank you!",
+                transactionSystem = "-",
+                transactionValue = "Rp25.000",
+            ),
+            Transaction(
+                id = 2,
+                transactionName = "Transfer",
+                userName = "Alice Smith",
+                date = "2023-09-17",
+                message = "Debt",
+                transactionSystem = "-",
+                transactionValue = "Rp250.000",
+            ),
+        )
+        val transactionAdapter = TransactionAdapter(transaction)
+        rvTransaction.adapter = transactionAdapter
+    }
+
+    private fun bindCategory(){
+        val rvKategori = binding.rvKategori
+        val category: List<Category> = listOf(
+            Category(
+                id = 1,
+                name = "Transfer",
+                description = "Kategori untuk transfer uang",
+                image = R.drawable.baseline_attach_money_24
+            ),
+            Category(
+                id = 2,
+                name = "Give Tip",
+                description = "Kategori untuk memberikan tips",
+                image = R.drawable.baseline_money_24
+            ),
+            Category(
+                id = 3,
+                name = "Top Up",
+                description = "Kategori untuk top up saldo",
+                image = R.drawable.baseline_shortcut_24
+            )
+        )
+
+        val categoryAdapter = CategoryAdapter(category)
+        rvKategori.adapter = categoryAdapter
+    }
+
+
+
+    override fun onCategoryItemClicked(category: Category) {
+        Log.d("MainActivity", "Category item clicked: ${category.name}")
+        when (category.id) {
+            1 -> {
+                // Tambahkan kode untuk berpindah ke activity Transfer
+                Toast.makeText(this@MainActivity, "Feature not yet available", Toast.LENGTH_LONG).show()
+            }
+            2 -> {
+                // Tambahkan kode untuk berpindah ke activity Give Tip
+                val intent = Intent(this@MainActivity, ListContactActivity::class.java)
+                startActivity(intent)
+            }
+            3 -> {
+                // Tambahkan kode untuk berpindah ke activity Top Up
+                Toast.makeText(this@MainActivity, "Feature not yet available", Toast.LENGTH_LONG).show()
+            }
+            else -> {
+                // Tindakan yang akan diambil jika kategori tidak dikenali
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
